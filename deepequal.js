2e68017af85775005e93552b66536a56d3d98efa
dhinakarankannan <dhinakarankannan15@gmail.com>
2017-07-06 16:31:33 -0400

    added some code in deepequal and strictdeepequal

--- deepequal.js
+++ deepequal.js
@@ -2,3 +2,4 @@ var assert = require('assert');
 var x = { a = { n: 0 } };
 var z = { a = { n: 1 } };
 assert.deepEqual(x, z, "My message goes here");
+assert.notDeepEqual(x, z, "My message goes here");
--- deepstrictequal.js
+++ deepstrictequal.js
@@ -4,3 +4,5 @@ var y = { a = { n: 0 } };
 var z = { a = { n: '0' } };
 assert.deepEqual(x, y); //OK
 assert.deepEqual(x, z); /*AssertionError: { a: { n: 0 } } deepStrictEqual {a: { n: '0' } }*/
+assert.notstrictDeepEqual(x, z, "My message goes here");
+assert.notstrictDeepEqual(x, y, "My message goes here");
